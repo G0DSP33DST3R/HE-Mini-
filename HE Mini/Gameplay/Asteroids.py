58af4a66754f1806e090008c55e3fc8fe028833e
Kaentoshan Development Agency <83178742+G0DSP33DST3R@users.noreply.github.com>
2026-09-15 07:29:32 -0500

    Add files via upload

---
+++ HE Mini/Gameplay/Asteroids.py
@@ -0,0 +1,12 @@
+# Asteroids.py
+import discord
+from discord.ext import commands
+
+
+class Asteroids(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Asteroids(bot))
